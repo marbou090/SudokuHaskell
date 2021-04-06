bd199c8d39178919319898d14265f9c16c164dd2
加藤まる <rikka2217@gmail.com>
2021-04-07 00:13:27 +0900

    finish lesson1

--- src/first_prog.hs
+++ src/first_prog.hs
@@ -1,5 +1,22 @@
-messyMain :: IO()
+messyMain::IO()
 messyMain = do
-    print "あなたはだあれ"
+    print "Who are you?"
     you <- getLine
-    print( "おまえは"++you)
+    print("You are " ++you)
+
+toPart recipient = "I am " ++ recipient ++",\n"
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+fromPart author = "Thanks,\n"++author
+
+createEmail recipient bookTitle author = toPart recipient ++
+                                        bodyPart bookTitle ++
+                                        fromPart author
+
+main = do   
+    print "Who is the mail for?"
+    recipient <- getLine
+    print "What is the Title?"
+    title <- getLine
+    print "Who is the Author?"
+    author <- getLine
+    print (createEmail recipient title author)
