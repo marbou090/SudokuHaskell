7556fd169014024049d888bf59a983298f68e61b
加藤まる <rikka2217@gmail.com>
2021-04-07 18:53:08 +0900

    lesson2 finish

---
+++ src/2_prog.hs
@@ -0,0 +1,9 @@
+simple x = x
+
+calcChange owed given = if given - owed > 0
+                        then given - owed
+                        else 0    
+    where change = given - owed
+
+doublePlusTwo x = dounleX + 2
+    where dounleX = x*2
