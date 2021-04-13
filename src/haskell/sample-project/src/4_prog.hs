ff78bd5864a0229ae08e8d11fb76b9b8059d0219
加藤まる <rikka2217@gmail.com>
2021-04-13 11:57:25 +0900

    finish lesson4

--- src/haskell/sample-project/src/4_prog.hs
+++ src/haskell/sample-project/src/4_prog.hs
@@ -49,11 +49,59 @@ compareLastNames name1 name2 =  if lastName1 > lastName2
                                 then GT
                                 else if lastName1 < lastName2
                                     then LT
-                                    else EQ
+                                    else if firstName1 > firstName2
+                                        then GT
+                                        else if firstName1 < firstName2
+                                            then LT
+                                            else EQ
     where   lastName1 = snd name1
             lastName2 = snd name2
+            firstName1 = fst name1
+            firstName2 = fst name2
 
 {-
 *Main> sortBy compareLastNames names
 [("Ian","Curtis"),("Peter","Hook"),("Stephen","Morris"),("Bernard","Sumner")]
+-}
+{-
+addressLetter name location = nameText ++ " - " ++ location
+    where nameText = (fst name) ++ " " ++ (snd name)
+-}
+
+{-
+*Main> addressLetter ("Bob","Smith") "PO Box 1234 - San Francisco, CA, 94111"
+"Bob Smith - PO Box 1234 - San Francisco, CA, 94111"
+-}
+addressLetter name location = locationFunction name
+    where locationFunction = getLOcationFunction location
+
+sfOffice name = if lastName < "L"
+                then nameText ++ " - PO Box 1234 - San Francisco, CA, 94111"
+                else nameText ++ " - PO Box 1010 - San Francisco, CA, 94109"
+    where   lastName = snd name
+            nameText = (fst name) ++ " " ++ lastName
+
+nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
+    where nameText = (fst name) ++ " " ++ (snd name)
+
+renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"
+    where nameText = snd name
+
+getLOcationFunction location = case location of
+    "ny" -> nyOffice
+    "sf" -> sfOffice
+    "reno" -> renoOffice
+    _ -> (\name -> (fst name) ++ " " ++ (snd name))
+
+{-
+*Main> addressLetter ("Bob","Smith") "ny"
+"Bob Smith: PO Box 789 - New York, NY, 10013"
+*Main> addressLetter ("Bob","Jones") "ny"
+"Bob Jones: PO Box 789 - New York, NY, 10013"
+*Main> addressLetter ("Samantha","Smith") "sf"
+"Samantha Smith - PO Box 1010 - San Francisco, CA, 94109"
+*Main> addressLetter ("Bob","Smith") "reno"
+"Smith - PO Box 456 - Reno, NV 89523"
+*Main> addressLetter ("Bob","Smith") "la"
+"Bob Smith"
 -}
